f4f3954ead0ea0380754ea4864e1b5d37900a84b
wachawo <wachawo@gmail.com>
2026-02-24 16:19:42 +0000

    feat: update run.sh to create virtual environment if not present and install dependencies

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -euxo
 cd "$(dirname "$0")"
-source /opt/venv/bin/activate
+if [ -z $VIRTUAL_ENV ]; then
+  if [ ! -d .venv ]; then
+    python3 -m venv .venv
+  fi
+  source .venv/bin/activate
+  pip3 install -r moshi/requirements.txt
+fi
 pip install -e moshi/.
 SSL_DIR=$(mktemp -d)
 python -m moshi.server --fp8 --ssl "$SSL_DIR"
